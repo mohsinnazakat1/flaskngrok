21dd4bec4c7c42f0b15f6754f2680b7a39c09449
mohsinnazakat1 <csworld7177@gmail.com>
2022-04-25 15:28:10 +0500

    updated the model and vectorizer resolved the issue of depricated pickle and other functions and updated code from application phase of jupyter notebook file

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import re
 import sklearn  
 import requests
 from pydub import AudioSegment
+from sklearn.feature_extraction.text import TfidfVectorizer 
 import io
 import os
 import requests
@@ -25,26 +26,48 @@ def predict():
 	audiofile = AudioSegment.from_file(io.BytesIO(audio), format="mp4")
 	audiofile.export(out_f="file.wav",format="wav")
 
+	#transcription module begins
+	
 	recognizer = sr.Recognizer()
 	recognizer.energy_threshold = 300
 	sample_file = sr.AudioFile('file.wav') 
-
+	#Convert AudioFile to AudioData
 	with sample_file as source:
+		#record the audio
 		sample_audio = recognizer.record(source)
-	transcription = recognizer.recognize_google(audio_data=sample_audio, language="en-US")
-	unseen_data = pd.DataFrame([(transcription)],columns=['Transcription'])
-	unseen_data['Transcription'] = unseen_data['Transcription'].apply(lambda x: " ".join(x.lower() for x in x.split())) 
-	unseen_data['Transcription'] = unseen_data['Transcription'].map(lambda x: re.sub(r'\W+', ' ', x)) 
-	vectorizer_word_unigram = pickle.load(open('./models/vectorizer_word_unigram.pkl', 'rb'))
-	unseen_data = unseen_data['Transcription']
-	transform_unseen_data = vectorizer_word_unigram.transform(unseen_data)
-	transform_unseen_data = transform_unseen_data.todense()
-	word_unigram_features = vectorizer_word_unigram.get_feature_names()
-	unseen_data_features = pd.DataFrame(transform_unseen_data, columns = word_unigram_features)
+		
+	#Transcribing urduSpeech_audio
+	trnascripted_text=recognizer.recognize_google(audio_data=sample_audio, language="en-US")
+
+	trnascripted_text_list=[trnascripted_text]
+	app_transcription=pd.DataFrame(trnascripted_text_list, columns=['Transcription'])
+	#text based preprocessing
+	app_transcription['Transcription'] = app_transcription['Transcription'].apply(lambda x: " ".join(x.lower() for x in x.split())) 
+	app_transcription['Transcription'] = app_transcription['Transcription'].map(lambda x: re.sub(r'\W+', ' ', x)) 
+	preprocessed_testing_data = app_transcription
+
+	#loading tfidf vetorizer 
+	Tfidf_vectorizer = pickle.load(open('./models/vectorizer_word_unigram.pkl', 'rb'))
+	# Input of Testing Data
+	test_text = preprocessed_testing_data['Transcription']
+
+	# Transform the Input Text using TFIDF Vectorizer
+	transform_features = Tfidf_vectorizer.transform(test_text)
+
+	# Get the name of Features (Feature  Set)
+	feature_set = Tfidf_vectorizer.get_feature_names_out()
+
+	# Convert Transformed features into Array and Create a Dataframe
+	Input_features = pd.DataFrame(transform_features.toarray(), columns = feature_set)
+
+	# Get the name of Features (Feature  Set) and create a DataFrame of Input Features
+	input_testing_features = pd.DataFrame(Input_features, columns = Tfidf_vectorizer.get_feature_names_out())
+	# Load the Saved Model
 	model = pickle.load(open('./models/trained_model.pkl', 'rb'))
-	model_predictions = model.predict(unseen_data_features)
+	model_predictions = model.predict(input_testing_features)
+
 	os.remove("file.wav")
 	return jsonify( 
 		result = int(model_predictions[0]),
-		transcription = transcription
+		transcription = trnascripted_text
 	)
